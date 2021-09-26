7d36c82c5698ed0e91db7b9b2e07efc80692cd07
chihiro <chihiro.tanigawa@accenture.com>
2021-09-27 02:03:08 +0900

    test9/27

--- site.config.js
+++ site.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  // rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '0dbd6121b8ed408095ebfe8a46b9b609',
+
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
